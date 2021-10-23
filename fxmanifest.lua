174fe6fe1c9ae2918b98ebfd5eaa3d135e059bb5
JulyGee <87626039+JulyGee@users.noreply.github.com>
2021-10-23 11:51:47 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+ui_page 'html/index.html'
+
+files{
+  'html/index.html',
+  'html/style.css',
+}
+
+client_scripts {
+  'config.lua',
+  'client/main.lua',
+}
+
+server_scripts {
+  'config.lua',
+  'server/main.lua',
+}
